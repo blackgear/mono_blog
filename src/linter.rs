5a7572b498c171540496bf44d8b2f9a17b0cbb67
Daniel <daniel65536@gmail.com>
2018-01-24 23:58:00 +0800

    remove useless alloc in linter

--- src/linter.rs
+++ src/linter.rs
@@ -5,10 +5,10 @@ use std::cmp::max;
 use std::sync::Mutex;
 use std::collections::HashMap;
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(PartialEq)]
 enum Scripts {
     Numbers,
-    Western,
+    English,
     Chinese,
     Unknown,
 }
@@ -45,57 +45,9 @@ lazy_static! {
 
 fn parse(ch: char) -> Scripts {
     match ch {
-        '\u{0030}'...'\u{0039}' |
-        '\u{10140}'...'\u{1018F}' |
-        '\u{10100}'...'\u{1013F}' => Scripts::Numbers,
+        '\u{0030}'...'\u{0039}' => Scripts::Numbers,
         '\u{0041}'...'\u{005A}' |
-        '\u{0061}'...'\u{007A}' |
-        '\u{00C0}'...'\u{00FF}' |
-        '\u{0100}'...'\u{017F}' |
-        '\u{0180}'...'\u{024F}' |
-        '\u{0250}'...'\u{02AF}' |
-        '\u{02B0}'...'\u{02FF}' |
-        '\u{0300}'...'\u{036F}' |
-        '\u{0370}'...'\u{03FF}' |
-        '\u{0400}'...'\u{04FF}' |
-        '\u{0500}'...'\u{052F}' |
-        '\u{0530}'...'\u{058F}' |
-        '\u{10A0}'...'\u{10FF}' |
-        '\u{1680}'...'\u{169F}' |
-        '\u{16A0}'...'\u{16FF}' |
-        '\u{1AB0}'...'\u{1AFF}' |
-        '\u{1C80}'...'\u{1C8F}' |
-        '\u{1D00}'...'\u{1D7F}' |
-        '\u{1D80}'...'\u{1DBF}' |
-        '\u{1DC0}'...'\u{1DFF}' |
-        '\u{1E00}'...'\u{1EFF}' |
-        '\u{1F00}'...'\u{1FFF}' |
-        '\u{20D0}'...'\u{20FF}' |
-        '\u{2C00}'...'\u{2C5F}' |
-        '\u{2C60}'...'\u{2C7F}' |
-        '\u{2D00}'...'\u{2D2F}' |
-        '\u{2DE0}'...'\u{2DFF}' |
-        '\u{A640}'...'\u{A69F}' |
-        '\u{A700}'...'\u{A71F}' |
-        '\u{A720}'...'\u{A7FF}' |
-        '\u{AB30}'...'\u{AB6F}' |
-        '\u{FB00}'...'\u{FB4F}' |
-        '\u{10000}'...'\u{1007F}' |
-        '\u{10080}'...'\u{100FF}' |
-        '\u{101D0}'...'\u{101FF}' |
-        '\u{10280}'...'\u{1029F}' |
-        '\u{102A0}'...'\u{102DF}' |
-        '\u{10300}'...'\u{1032F}' |
-        '\u{10330}'...'\u{1034F}' |
-        '\u{10350}'...'\u{1037F}' |
-        '\u{10450}'...'\u{1047F}' |
-        '\u{10500}'...'\u{1052F}' |
-        '\u{10530}'...'\u{1056F}' |
-        '\u{10600}'...'\u{1077F}' |
-        '\u{10800}'...'\u{1083F}' |
-        '\u{10920}'...'\u{1093F}' |
-        '\u{10C80}'...'\u{10CFF}' |
-        '\u{1E000}'...'\u{1E02F}' => Scripts::Western,
+        '\u{0061}'...'\u{007A}' => Scripts::English,
         '\u{3400}'...'\u{4DBF}' |
         '\u{4E00}'...'\u{9FFF}' |
         '\u{F900}'...'\u{FAFF}' |
@@ -169,22 +121,19 @@ pub fn process<'a, S: AsRef<str>>(content: S) -> Cow<'a, str> {
 
     while let Some(ch) = chars.next() {
         let ns = parse(ch);
-        match (ws, ns) {
-            (Scripts::Chinese, Scripts::Western) |
-            (Scripts::Chinese, Scripts::Numbers) |
-            (Scripts::Numbers, Scripts::Chinese) => {
+        if ws != ns {
+            if ws == Scripts::Chinese && ns != Scripts::Unknown {
                 result.push('\u{2009}');
             }
-            (Scripts::Western, Scripts::Chinese) => {
-                if !buffer.is_empty() {
-                    result.push_str(hyphen(&buffer).as_ref());
-                    buffer.clear();
-                }
+            if ws == Scripts::English {
+                result.push_str(hyphen(&buffer).as_ref());
+                buffer.clear();
+            }
+            if ns == Scripts::Chinese && ws != Scripts::Unknown {
                 result.push('\u{2009}');
             }
-            _ => {}
         }
-        if ns == Scripts::Western {
+        if ns == Scripts::English {
             buffer.push(ch);
         } else {
             result.push(ch);
